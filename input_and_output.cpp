112e1189ef3fbec3454227da3d0f1c47914447b6
Sinthia666 <85312355+Sinthia666@users.noreply.github.com>
2021-06-10 10:20:25 +0600

    Add files via upload

---
+++ input_and_output.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+
+    cin >> a >> b >> c;   // scanf("%d %d %d", &a , &b, &c);
+    
+
+    cout << a + b + c << endl;     //printf("%d", a+b+c);
+
+   
+}
