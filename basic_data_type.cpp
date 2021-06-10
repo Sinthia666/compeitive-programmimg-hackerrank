8dcbaffe00ca2110ad414488a5657b02946edb50
Sinthia666 <85312355+Sinthia666@users.noreply.github.com>
2021-06-10 10:24:22 +0600

    Add files via upload

---
+++ basic_data_type.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long b;
+    char ch;
+    float f;
+    double d;
+    cin >> a >> b >> ch >> f >> d;
+    cout << a << endl;
+    cout << b << endl;
+    cout << ch << endl;
+    printf("%.3f\n%.9lf\n", f, d);
+}
